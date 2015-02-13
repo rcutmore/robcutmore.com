e9a60b4f73a3bccbe3857e8cc807ce3eb0f83bd4
Rob Cutmore <robcutmore@gmail.com>
2015-02-13 18:22:41 -0500

    Add initial about page.

--- mysite/settings.py
+++ mysite/settings.py
@@ -82,3 +82,12 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Template files
+
+TEMPLATE_PATH = os.path.join(BASE_DIR, 'templates')
+
+TEMPLATE_DIRS = (
+    TEMPLATE_PATH,
+)
--- mysite/urls.py
+++ mysite/urls.py
@@ -5,5 +5,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^about/$', 'mysite.views.about', name='about'),
     url(r'^blog/', include('blog.urls')),
 )
