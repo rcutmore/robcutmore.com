c5b744a56453d3d08ec22ed761e3b43c2e7b52bb
Rob Cutmore <robcutmore@gmail.com>
2015-02-14 10:51:41 -0500

    Re-organize app structure.

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,10 +2,9 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'mysite.views.home', name='home'),
+    url(r'^$', include('apps.blog.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^about/$', 'mysite.views.about', name='about'),
-    url(r'^blog/', include('blog.urls')),
-    url(r'^portfolio/', include('portfolio.urls')),
+    url(r'^blog/', include('apps.blog.urls')),
+    url(r'^portfolio/', include('apps.portfolio.urls')),
 )
