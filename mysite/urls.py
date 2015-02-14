ba796f4a8d6f3b9bac0209aa0be8fc5e32b7ea87
Rob Cutmore <robcutmore@gmail.com>
2015-02-14 11:22:27 -0500

    Update to use a base template for all other templates.

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,9 +2,9 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    url(r'^$', include('apps.blog.urls')),
+    url(r'^$', include('apps.blog.urls', namespace='blog')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^about/$', 'mysite.views.about', name='about'),
-    url(r'^blog/', include('apps.blog.urls')),
-    url(r'^portfolio/', include('apps.portfolio.urls')),
+    url(r'^blog/', include('apps.blog.urls', namespace='blog')),
+    url(r'^portfolio/', include('apps.portfolio.urls', namespace='portfolio')),
 )
