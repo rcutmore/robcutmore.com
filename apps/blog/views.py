276277e19ec7de4516ba4b1f93797d4ca3185816
Rob Cutmore <robcutmore@gmail.com>
2015-02-15 11:41:50 -0500

    Update to display blog posts in reverse order.

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -3,7 +3,7 @@ from .models import Post
 
 def post_list(request):
     posts = Post.objects.filter(
-        published_date__isnull=False).order_by('published_date')
+        published_date__isnull=False).order_by('-published_date')
     context_dict = {'posts': posts, 'active_page': 'blog'}
 
     return render(request, 'blog/post_list.html', context_dict)
