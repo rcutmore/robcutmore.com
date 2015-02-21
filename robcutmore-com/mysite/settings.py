b7275be4e7b2f9947149f51966b4260914a32597
Rob Cutmore <robcutmore@gmail.com>
2015-02-21 11:51:12 -0500

    Update settings for serving static files.

--- robcutmore-com/mysite/settings.py
+++ robcutmore-com/mysite/settings.py
@@ -87,7 +87,7 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_ROOT = 'myenv/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_final')
 
 STATIC_URL = '/static/'
 
