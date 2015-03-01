77f15e1198820f01d27b6ec851d600b5f0972f3f
Rob Cutmore <robcutmore@gmail.com>
2015-03-01 09:38:15 -0500

    Use register decorator in blog admin file.

--- robcutmore-com/apps/blog/admin.py
+++ robcutmore-com/apps/blog/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from .models import Post, PostTag
 
+@admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     filter_horizontal = ('tags',)
 
-admin.site.register(Post, PostAdmin)
 admin.site.register(PostTag)
