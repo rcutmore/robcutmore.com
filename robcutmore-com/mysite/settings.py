72d69dbadda3bfa1d89fbbf6b60c7aa432798915
Rob Cutmore <robcutmore@gmail.com>
2015-03-07 13:02:13 -0500

    Fix settings.py not getting booleans properly from settings file.

--- robcutmore-com/mysite/settings.py
+++ robcutmore-com/mysite/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config.get('project', 'SECRET_KEY')
+SECRET_KEY = config.getboolean('project', 'SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config.get('project', 'DEBUG')
+DEBUG = config.getboolean('project', 'DEBUG')
 
 TEMPLATE_DEBUG = config.get('project', 'TEMPLATE_DEBUG')
 
