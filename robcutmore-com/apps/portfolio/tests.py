fc7fa32b0fc90c14885ff88ec09030976a2011b9
Rob Cutmore <robcutmore@gmail.com>
2015-03-14 12:32:14 -0400

    Add tests for portfolio_tags.

--- robcutmore-com/apps/portfolio/tests.py
+++ robcutmore-com/apps/portfolio/tests.py
@@ -2,19 +2,91 @@ from django.core.urlresolvers import reverse
 from django.test import TestCase
 
 from .models import Project, ProjectTag
+from .templatetags.portfolio_tags import get_project_list
 
 def add_project_tag(title):
     tag = ProjectTag.objects.get_or_create(title=title)[0]
     return tag
 
-def add_project(title, description, url, tags):
+def add_project(title, description, url='http://test.com', tags=None):
     project = Project.objects.get_or_create(
         title=title, description=description, url=url)[0]
 
+    tags = tags if tags else []
     project_tags = [add_project_tag(tag) for tag in tags]
     project.tags.add(*project_tags)
 
     return project
+
+class PortfolioTagsTests(TestCase):
+    def test_get_project_list_with_no_projects(self):
+        """get_project_list should return no projects when none exist."""
+        result = get_project_list()
+
+        self.assertQuerysetEqual(result['projects'].object_list, [])
+        self.assertIsNone(result['tag'])
+        self.assertFalse(result['filtered'])
+
+    def test_get_project_list_with_projects(self):
+        """get_project_list should return existing projects."""
+        first_project = add_project(title='1', description='1')
+        second_project = add_project(title='2', description='2')
+
+        result = get_project_list()
+
+        projects = [first_project, second_project]
+        result_projects = result['projects'].object_list
+
+        for project in projects:
+            self.assertTrue(project in result_projects)
+        self.assertEqual(len(result_projects), len(projects))
+
+        self.assertIsNone(result['tag'])
+        self.assertFalse(result['filtered'])
+
+    def test_get_project_list_with_tag_filter(self):
+        """get_project_list should return only projects with given tag."""
+        tag = 'tag1'
+        first_project = add_project(title='1', description='1', tags=[tag])
+        second_project = add_project(title='2', description='2', tags=[tag])
+        project_without_tag = add_project(title='3', description='3', tags=[])
+
+        result = get_project_list(tag=tag)
+
+        projects_with_tag = [first_project, second_project]
+        result_projects = result['projects'].object_list
+
+        for project_with_tag in projects_with_tag:
+            self.assertTrue(project_with_tag in result_projects)
+        self.assertFalse(project_without_tag in result_projects)
+        self.assertEqual(len(result_projects), len(projects_with_tag))
+
+        self.assertTrue(result['filtered'])
+        self.assertEqual(result['tag'], tag)
+
+    def test_get_project_list_with_page_filter(self):
+        """get_project_list should return only projects for given page."""
+        projects = [add_project(title=str(i), description=str(i)) for i in range(10)]
+        first_page_projects = projects[:5]
+        second_page_projects = projects[5:]
+
+        # Test first page.
+        result = get_project_list(page=1)
+        first_page_result_projects = result['projects'].object_list
+
+        for first_page_project in first_page_projects:
+            self.assertTrue(first_page_project in first_page_result_projects)
+        for second_page_project in second_page_projects:
+            self.assertFalse(second_page_project in first_page_result_projects)
+
+        # Test second page.
+        result = get_project_list(page=2)
+        second_page_result_projects = result['projects'].object_list
+
+        for first_page_project in first_page_projects:
+            self.assertFalse(first_page_project in second_page_result_projects)
+        for second_page_project in second_page_projects:
+            self.assertTrue(second_page_project in second_page_result_projects)
 
 class ProjectListTests(TestCase):
     def test_project_list_with_no_projects(self):
@@ -26,10 +98,8 @@ class ProjectListTests(TestCase):
 
     def test_project_list_with_projects(self):
         """project_list should display all projects."""
-        first_project = add_project(
-            'Project 1', 'Project 1 description', 'http://www.robcutmore.com', [])
-        second_project = add_project(
-            'Project 2', 'Project 2 description', 'http://www.robcutmore.com', [])
+        first_project = add_project(title='1', description='1')
+        second_project = add_project(title='2', description='2')
 
         response = self.client.get(reverse('portfolio:project_list'))
 
@@ -42,8 +112,7 @@ class ProjectListTests(TestCase):
     def test_project_list_tags(self):
         """project_list should display project tags."""
         tags = ['tag1', 'tag2', 'tag3']
-        project = add_project(
-            'Project 1', 'Project 1 description', 'http://robcutmore.com', tags)
+        project = add_project(title='1', description='1', tags=tags)
 
         response = self.client.get(reverse('portfolio:project_list'))
 
