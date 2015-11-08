2eb335dd7ae60130e02debde3ceecc07f92c620a
Rob Cutmore <robcutmore@gmail.com>
2015-11-08 12:07:42 -0500

    Reformat urls.py for blog and portfolio apps
    Reformatted code in urls.py for blog and portfolio apps. Added
    comments and removed call to deprecated django.conf.urls.patterns
    method.

--- robcutmore-com/apps/blog/urls.py
+++ robcutmore-com/apps/blog/urls.py
@@ -1,9 +1,30 @@
-from django.conf.urls import include, patterns, url
+from django.conf.urls import url
+
 from . import views
 
-urlpatterns = patterns('',
-    url(r'^$', views.post_list, name='post_list'),
-    url(r'^filter/$', views.filter_post_list, name='filter_post_list'),
-    url(r'^(?P<tag>[\w\-]+)/$', views.post_list, name='post_list_filtered'),
-    url(r'^(?P<post_year>[\d]+)/(?P<post_month>[\d]+)/(?P<post_day>[\d]+)/(?P<post_slug>[\w\-]+)/$', views.post_detail, name='post_detail'),
-)
+
+urlpatterns = [
+    # Ex: /blog/
+    # This lists all blog posts.
+    url(r'^$',
+        views.post_list,
+        name='post_list'),
+
+    # Ex: /blog/filter/
+    # Used by Ajax calls to filter/unfilter list of blog posts.
+    url(r'^filter/$',
+        views.filter_post_list,
+        name='filter_post_list'),
+
+    # Ex: /blog/python/
+    # This would list all blog posts with the "python" tag.
+    url(r'^(?P<tag>[\w\-]+)/$',
+        views.post_list,
+        name='post_list_filtered'),
+
+    # Ex: /blog/2015/8/27/example-blog-post/
+    # This would show a particular blog post that was posted 2015/8/27.
+    url(r'^(?P<post_year>[\d]+)/(?P<post_month>[\d]+)/(?P<post_day>[\d]+)/(?P<post_slug>[\w\-]+)/$',
+        views.post_detail,
+        name='post_detail'),
+]
--- robcutmore-com/apps/portfolio/urls.py
+++ robcutmore-com/apps/portfolio/urls.py
@@ -1,8 +1,24 @@
-from django.conf.urls import include, patterns, url
+from django.conf.urls import url
+
 from . import views
 
-urlpatterns = patterns('',
-    url(r'^$', views.project_list, name='project_list'),
-    url(r'^filter/$', views.filter_project_list, name='filter_project_list'),
-    url(r'^(?P<tag>[\w\-]+)/$', views.project_list, name='project_list_filtered'),
-)
+
+urlpatterns = [
+    # Ex: /portfolio/
+    # This lists all portfolio projects.
+    url(r'^$',
+        views.project_list,
+        name='project_list'),
+
+    # Ex: /portfolio/filter/
+    # Used by Ajax calls to filter/unfilter list of portfolio projects.
+    url(r'^filter/$',
+        views.filter_project_list,
+        name='filter_project_list'),
+
+    # Ex: /portfolio/python/
+    # This would list all portfolio projects with the "python" tag.
+    url(r'^(?P<tag>[\w\-]+)/$',
+        views.project_list,
+        name='project_list_filtered'),
+]
