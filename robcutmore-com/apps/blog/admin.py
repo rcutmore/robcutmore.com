fcbd67eeb6fa4433459e4f504afaf68af87c53c5
Rob Cutmore <robcutmore@gmail.com>
2015-11-08 10:47:07 -0500

    Reformat admin.py for blog and portfolio apps

--- robcutmore-com/apps/blog/admin.py
+++ robcutmore-com/apps/blog/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
+
 from .models import Post, PostTag
 
+
+# Customize appearance of Post's tags field on admin site.
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     filter_horizontal = ('tags',)
--- robcutmore-com/apps/portfolio/admin.py
+++ robcutmore-com/apps/portfolio/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
+
 from .models import Project, ProjectTag
 
+
+# Customize appearance of Project's tags field on admin site.
 @admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
     filter_horizontal = ('tags',)
