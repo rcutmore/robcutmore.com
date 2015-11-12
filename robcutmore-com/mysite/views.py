eb515b4c63e99d90d16d147d73bccc543ac3511a
Rob Cutmore <robcutmore@gmail.com>
2015-11-12 07:45:15 -0500

    Improve documentation for main project views

--- robcutmore-com/mysite/views.py
+++ robcutmore-com/mysite/views.py
@@ -1,4 +1,14 @@
+"""
+Contains main project views.
+"""
 from django.shortcuts import render
 
+
 def about(request):
+    """ Render HTML for about page.
+
+    :param request: :class:`HttpRequest` object to generate response
+        with.
+    :returns: Rendered :class:`HttpResponse` object.
+    """
     return render(request, 'about.html', {'active_page': 'about'})
