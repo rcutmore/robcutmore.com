6f6add9f49fa73a893bb4b9ba38ff6b7390550ef
Rob Cutmore <robcutmore@gmail.com>
2015-11-14 09:20:39 -0500

    Update virtualenv name

--- robcutmore-com/bin/start_server.sh
+++ robcutmore-com/bin/start_server.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 project_dir=/home/rc/projects/robcutmore
-gunicorn_location=$project_dir/myenv/bin/gunicorn
+gunicorn_location=$project_dir/env/bin/gunicorn
 gunicorn_config=$project_dir/robcutmore-com/config/gunicorn.py
 $gunicorn_location \
     --config=$gunicorn_config \
--- robcutmore-com/config/gunicorn.py
+++ robcutmore-com/config/gunicorn.py
@@ -1,4 +1,4 @@
-command = '/home/rc/projects/robcutmore/myenv/bin/gunicorn'
+command = '/home/rc/projects/robcutmore/env/bin/gunicorn'
 pythonpath = '/home/rc/projects/robcutmore/robcutmore-com'
 bind = '127.0.0.1:8001'
 workers = 1
