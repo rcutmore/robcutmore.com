6853dd50b6e9fdc76498ec71e63e765c600a61c9
Rob Cutmore <robcutmore@gmail.com>
2016-03-25 18:46:30 -0400

    Use new template settings

--- robcutmore-com/mysite/settings.py
+++ robcutmore-com/mysite/settings.py
@@ -104,8 +104,23 @@ STATICFILES_DIRS = (
 
 # Template files
 
-TEMPLATE_PATH = os.path.join(BASE_DIR, 'templates')
-
-TEMPLATE_DIRS = (
-    TEMPLATE_PATH,
-)
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.debug',
+                'django.template.context_processors.i18n',
+                'django.template.context_processors.media',
+                'django.template.context_processors.static',
+                'django.template.context_processors.tz',
+                'django.contrib.messages.context_processors.messages',
+            ]
+        }
+    }
+]
